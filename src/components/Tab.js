08ce60cdb102102a3f1199665e28ca0099314cae
dhanu <dhanunjai01@gmail.com>
2023-06-15 01:48:27 +0530

    done

---
+++ src/components/Tab.js
@@ -0,0 +1,26 @@
+import React,{useState} from 'react'
+
+const Tab = ({tabs}) => {
+
+    const[tabClicked,setTabClicked] = useState("");
+
+    function handleClick(content){
+          setTabClicked(content)
+    }
+
+  return (
+    <div>
+        <ul>
+       {
+         tabs.map((tab)=>(
+             <li onClick={()=>handleClick(tab.content)}>{tab.title}</li>
+         ))
+       }
+       </ul>
+
+       {tabClicked && <p>{tabClicked}</p>}
+    </div>
+  )
+}
+
+export default Tab
